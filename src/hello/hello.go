10a2c69f260c6c8daede4d2de47dd90398a2ed0e
joelanderson000 <joelanderson000@yahoo.ca>
2019-02-23 17:26:36 -0600

    play with for loop and return calls

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -3,5 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println ("Hello World!")
+	fmt.Println("Hello World!")
+	fmt.Println(breakString("Hello World!"))
+}
+
+func breakString(inString string) string {
+	for i := 0; i < len(inString); i++ {
+		fmt.Println(string(inString[i]))
+	}
+	return "Successfully printed characters"
 }
